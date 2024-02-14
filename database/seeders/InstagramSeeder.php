7fb41aa87f3919e647b1b25580e46386088a9a18
unknown <barasatyaradi@gmail.com>
2024-02-14 17:21:05 +0800

    Update database

--- database/seeders/InstagramSeeder.php
+++ database/seeders/InstagramSeeder.php
@@ -22,7 +22,7 @@ class InstagramSeeder extends Seeder
             'posts' => 49,
             'follower' => 364,
             'following' => 161,
-            'pfp' => "https://scontent.cdninstagram.com/v/t51.2885-19/325422530_997582474548063_2116158990505916380_n.jpg?stp=dst-jpg_s100x100&_nc_cat=109&ccb=1-7&_nc_sid=3fd06f&_nc_ohc=2E4b1_HbuGkAX8IiwzP&_nc_ht=scontent.cdninstagram.com&oh=00_AfBM3Ah-yjxpSNgmjG17pVY0OUOHe6mJ96s_DLzZ_jRilg&oe=65BB68E0",
+            'pfp' => "https://scontent.cdninstagram.com/v/t51.2885-19/325422530_997582474548063_2116158990505916380_n.jpg?stp=dst-jpg_s100x100&_nc_cat=109&ccb=1-7&_nc_sid=3fd06f&_nc_ohc=OdT1noq-IeAAX9qNe2G&_nc_ht=scontent.cdninstagram.com&oh=00_AfDYWK_opFEBJd6vGHiuYgiPX8gMXr0F_NSfKcDbo1H25g&oe=65D129A0",
         ]);
     }
 }
