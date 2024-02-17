067fcc8b9452bed1411ac45cb0916b0cae9c0777
Tika~ <barasatyaradi@gmail.com>
2024-02-17 13:58:46 +0700

    add website template

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,19 +1,20 @@
 @extends('template.template')
-
 @section('title', "Plushiee's Minecraft Map")
+
 @section('css')
     <link rel="stylesheet" href="/css/map.css">
 @endsection
 
 @section('main')
-    <noscript>
-        For full functionality of this site it is necessary to enable JavaScript.
-        Here are the <a href="http://www.enable-javascript.com/" target="_blank">
-            instructions how to enable JavaScript in your web browser</a>.
-    </noscript>
-    <div class="container-fluid main px-0">
-        <iframe src="{{ route('minecraft.map') }}" frameborder="0"></iframe>
-    </div>
+    <main>
+        <noscript>
+            For full functionality of this site it is necessary to enable JavaScript.
+            Here are the <a href="http://www.enable-javascript.com/" target="_blank">
+                instructions how to enable JavaScript in your web browser</a>.
+        </noscript>
+        <div class="container-fluid main px-0">
+            <iframe src="{{ route('minecraft.map') }}" frameborder="0"></iframe>
+        </div>
     </main>
 @endsection
 
